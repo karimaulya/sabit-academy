397395237e59e41fdb424dc0ff8fb22970e1fe80
karimaulya <karimaulya13@gmail.com>
2025-05-20 20:58:58 +0700

    sabit

---
+++ sabit.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int detik, menit, jam, parkir, totaldetik;
+    string kendaraan;
+
+    while(kendaraan != "stop") {
+
+        cout << "masukkan jenis kendaraan: ";
+        cin >> kendaraan;
+        cout << "masukkan waktu detik: ";
+        cin >> totaldetik;
+    
+        jam = totaldetik/3600;
+        menit = (totaldetik%3600)/60;
+        detik = (totaldetik%3600)%60;
+    
+        if (kendaraan == "mobil") {
+            if (totaldetik <= 3600) {
+                parkir = 1000;
+            } else {
+                parkir = 1000 + (jam*3000);
+            
+            }
+    
+        } else if (kendaraan == "motor") {
+            if (totaldetik <= 3600) {
+                parkir = 1000;
+            } else {
+                parkir = 1000 + (jam*2000);
+            
+            }
+
+        } else if (kendaraan == "stop") {
+            break;
+        }
+
+        cout << "jenis kendaraan: " << kendaraan << endl;
+        cout << "waktu parkir: " << jam << " jam " << menit << " menit " << detik << " detik" << endl;
+        cout << "total biaya parkir: " << parkir << endl;
+    }
+
+    return 0;
+}
